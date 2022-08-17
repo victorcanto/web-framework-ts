2d3f71271da7078f5435e6195096b1a9233b21b9
Victor Canto <iamvictorcanto@gmail.com>
2022-08-17 15:40:18 -0300

    feat: mapping regions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 
 const user = User.buildUser({ name: 'NAME', age: 25 });
 const rootEl = document.getElementById('root');
 
 if (rootEl) {
-  const userForm = new UserForm(rootEl, user);
-  userForm.render();
+  const userEdit = new UserEdit(rootEl, user);
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found.');
 }
